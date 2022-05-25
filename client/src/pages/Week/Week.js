61a9cdbf72e72452ed8f4160979e683ff8a59c0d
Semir Iljazi <iljazi.semir22@gmail.com>
2022-05-25 17:38:43 +0200

    Style of Week Page

--- client/src/pages/Week/Week.js
+++ client/src/pages/Week/Week.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Step from "../../components/Step";
 import "../../style/week.css";
 import { useParams } from "react-router-dom";
+import Home from "../../style/SVG/Home";
 
 export default function Week(props) {
   const { weekId } = useParams();
@@ -49,7 +50,9 @@ export default function Week(props) {
 
   return (
     <div className="week">
-      <Link to="/">Home</Link>
+      <Link to="/">
+        <Home className="homeSvg" />
+      </Link>
       <h1>{week.name}</h1>
       {week.steps.map((text) => (
         <Step text={text} />
