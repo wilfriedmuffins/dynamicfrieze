43546cdd6faf4e21572ada2aed6d1ec0d5e14818
Semir Iljazi <iljazi.semir22@gmail.com>
2022-05-25 15:05:34 +0200

    Frieze update

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 import WeekInFrieze from "./components/WeekInFrieze/WeekInFrieze";
 
 function App() {
+  const [weeks, setWeeks] = useState([
+    {
+      name: "Semaine 1",
+      steps: ["La voie du codeur 2.0", "Prendre un bon départ"],
+    },
+    {
+      name: "Semaine 2",
+      steps: ["La voie du codeur 2.0", "Prendre un bon départ"],
+    },
+    {
+      name: "Semaine 3",
+      steps: ["La voie du codeur 2.0", "Prendre un bon départ"],
+    },
+    {
+      name: "Semaine 4",
+      steps: ["La voie du codeur 2.0", "Prendre un bon départ"],
+    },
+    {
+      name: "Semaine 5",
+      steps: ["La voie du codeur 2.0", "Prendre un bon départ"],
+    },
+    {
+      name: "Semaine 6",
+      steps: ["La voie du codeur 2.0", "Prendre un bon départ"],
+    },
+  ]);
   return (
     <div className="App">
       <h1>Dynamic Frieze</h1>
       <div className="frieze">
-        <WeekInFrieze />
-        <WeekInFrieze />
-        <WeekInFrieze />
-        <WeekInFrieze />
-        <WeekInFrieze />
+        {weeks.map((week) => (
+          <WeekInFrieze week={week} setWeeks={setWeeks} />
+        ))}
       </div>
     </div>
   );
